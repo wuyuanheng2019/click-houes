de06b075130543024f37b4d7c9e4b3a921f380d3
wuyuanheng2019 <wyc452092540>
2021-05-23 17:22:17 +0800

    项目初始化

---
+++ click-web/src/main/java/com/clickhoues/ClickHouesApplication.java
@@ -0,0 +1,11 @@
+package com.clickhoues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickHouesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClickHouesApplication.class, args);
+    }
+}
